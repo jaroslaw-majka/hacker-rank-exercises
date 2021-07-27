3424e8359aea9152a95e3c7d4e2b59037f92db9e
Jarek Majka <majka.jaroslaw@gmail.com>
2021-07-27 21:32:26 +0200

    Finding the percentage solved

--- Finding the percentage.py
+++ Finding the percentage.py
@@ -9,10 +9,16 @@
 # length of marks arrays = 3
 
 if __name__ == '__main__':
-    n = int(input())
+    n = int(input("How many students would you like to add? "))
     student_marks = {}
     for _ in range(n):
-        name, *line = input().split()
+        name, *line = input("Please give me their name and score in one line: ").split()
         scores = list(map(float, line))
         student_marks[name] = scores
-    query_name = input()
+    query_name = input("Which student's average would you like to see? ")
+
+    score_sum = 0.0
+    for i in range(len(student_marks[query_name])):
+        score_sum += student_marks[query_name][i]
+    average = score_sum / len(student_marks[query_name])
+    print("%.2f" % average)

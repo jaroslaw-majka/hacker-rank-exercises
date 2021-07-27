365fc9cc11c2496d83abf7dcfc5cda439788e16c
Jarek Majka <majka.jaroslaw@gmail.com>
2021-07-27 21:41:51 +0200

    No idea initial commit

---
+++ No idea.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/no-idea/problem?h_r=next-challenge&h_v=zen
+
+# here is an array of n integers.
+# There are also  disjoint sets, A and B, each containing m integers.
+# You like all the integers in set A and dislike all the integers in set B.
+# Your initial happiness is 0.
+# For each  integer in the array, if i in A, you add 1 to your happiness.
+# If i in B, you add -1 to your happiness.
+# Otherwise, your happiness does not change. Output your final happiness at the end.
+

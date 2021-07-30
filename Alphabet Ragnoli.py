2120ae7c21634a363faeb3ea274236e04d583178
Jarek Majka <majka.jaroslaw@gmail.com>
2021-07-30 21:41:07 +0200

    Alphabet Rangoli initial commit

---
+++ Alphabet Ragnoli.py
@@ -0,0 +1,8 @@
+# Link: https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+
+def print_rangoli(size):
+    # your code goes here
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)

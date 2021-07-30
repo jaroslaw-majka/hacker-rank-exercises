7a8fa25a4fa0ff9f6bfa3f6a28f7f2cdfd27ce56
Jarek Majka <majka.jaroslaw@gmail.com>
2021-07-30 14:19:07 +0200

    String formatting initial commit

---
+++ String Formatting.py
@@ -0,0 +1,14 @@
+# Link: https://www.hackerrank.com/challenges/python-string-formatting/problem
+
+# Given an integer,n , print the following values for each integer i from 1 to n:
+# Decimal
+# Octal
+# Hexadecimal
+# Binary
+
+def print_formatted(number):
+    # your code goes here
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)

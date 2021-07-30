2dd08b1d091b2a1508ffde682ced57d4e3cc910a
Jarek Majka <majka.jaroslaw@gmail.com>
2021-07-30 22:08:23 +0200

    Alphabet Ragnoli solved

--- Alphabet Ragnoli.py
+++ Alphabet Ragnoli.py
@@ -1,7 +1,14 @@
 # Link: https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+import string
 
 def print_rangoli(size):
     # your code goes here
+    alpha = string.ascii_lowercase
+    L = []
+    for i in range(size):
+        s = "-".join(alpha[i:size])
+        L.append((s[::-1] + s[1:]).center(4 * n - 3, "-"))
+    print('\n'.join(L[:0:-1] + L))
 
 if __name__ == '__main__':
     n = int(input())

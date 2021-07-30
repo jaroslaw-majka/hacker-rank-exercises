110ffff65786a5d81e8d634406575e9ff3cd2615
Jarek Majka <majka.jaroslaw@gmail.com>
2021-07-30 14:32:01 +0200

    String Formatting solved

--- String Formatting.py
+++ String Formatting.py
@@ -7,7 +7,14 @@
 # Binary
 
 def print_formatted(number):
-    # your code goes here
+    l = len(bin(number)) - 2
+    for i in range(1, number + 1):
+        f = ""
+        for c in "doXb":
+            if f:
+                f += " "
+            f += "{:>" + str(l) + c + "}"
+        print(f.format(i, i, i, i))
 
 if __name__ == '__main__':
     n = int(input())

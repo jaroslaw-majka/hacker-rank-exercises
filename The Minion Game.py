4ccbf789ecabc9463c473c58dd2e04a1dd94d054
Jarek Majka <majka.jaroslaw@gmail.com>
2021-08-05 21:24:46 +0200

    Minion Game initial commit

---
+++ The Minion Game.py
@@ -0,0 +1,11 @@
+# Link: https://www.hackerrank.com/challenges/the-minion-game/problem
+
+# Vowels: AEIOU
+
+def minion_game(string):
+    # your code goes here
+
+if __name__ == '__main__':
+    # s = input()
+    s = "BANANA"
+    minion_game(s)

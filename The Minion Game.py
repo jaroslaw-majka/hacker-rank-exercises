702fa5b2254f19b30eef0fda55ab28afcce2648f
Jarek Majka <majka.jaroslaw@gmail.com>
2021-08-05 21:42:13 +0200

    Minion game solution

--- The Minion Game.py
+++ The Minion Game.py
@@ -1,11 +1,21 @@
 # Link: https://www.hackerrank.com/challenges/the-minion-game/problem
 
-# Vowels: AEIOU
-
 def minion_game(string):
-    # your code goes here
+    vowels = "AEIOU"
+    kevin_score, stuart_score = 0, 0
+    for i in range(len(string)):
+        score = len(string) - i
+        if string[i] in vowels:
+            kevin_score += score
+        else:
+            stuart_score += score
+    if kevin_score > stuart_score:
+        print("Kevin {}".format(kevin_score))
+    elif kevin_score < stuart_score:
+        print("Stuart {}".format(stuart_score))
+    else:
+        print("Draw")
 
 if __name__ == '__main__':
-    # s = input()
-    s = "BANANA"
+    s = input()
     minion_game(s)

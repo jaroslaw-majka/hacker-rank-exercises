13ad0ad7e77fffc8869e42eef60244c6d6e7c29e
Jarek Majka <majka.jaroslaw@gmail.com>
2021-08-05 21:48:28 +0200

    Merge tools initial commit

---
+++ Merge the tools.py
@@ -0,0 +1,8 @@
+# Link: https://www.hackerrank.com/challenges/merge-the-tools/problem?h_r=next-challenge&h_v=zen
+
+def merge_the_tools(string, k):
+    # your code goes here
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)

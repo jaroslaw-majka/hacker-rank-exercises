16d4998ae477511a547677566f0bd40c5d35a7de
Jarek Majka <majka.jaroslaw@gmail.com>
2021-08-10 21:43:37 +0200

    set.add quiz solved

--- set.add.py
+++ set.add.py
@@ -1,2 +1,8 @@
 # Link: https://www.hackerrank.com/challenges/py-set-add/problem
 
+if __name__ == "__main__":
+    number_of_entries = int(input())
+    entries_set = set()
+    for i in range(number_of_entries):
+        entries_set.add(input())
+    print(len(entries_set))
